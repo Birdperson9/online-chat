e23ddcc0bdba3857395ad396129847dfffc764cc
Birdperson9 <yorieshkin@gmail.com>
2023-07-08 17:37:03 +0200

    add styles

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -6,6 +6,8 @@ import {
 } from '../appwriteConfig'
 
 const Room = () => {
+  const [messages, setMessages] = useState([])
+
   useEffect(() => {
     getMessages()
   }, [])
@@ -15,10 +17,29 @@ const Room = () => {
       DATABASE_ID,
       COLLECTION_ID_MESSAGES
     )
-    console.log(response)
+    setMessages(response.documents)
   }
 
-  return <div>Room</div>
+  return (
+    <main className='container'>
+      <div className='room--container'>
+        <div>
+          {messages.map((message) => (
+            <div key={message.$id} className='message-wrapper'>
+              <div className='message--header'>
+                <small className='message-timestamp'>
+                  {message.$createdAt}
+                </small>
+              </div>
+              <div className='message--body'>
+                <span>{message.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default Room
