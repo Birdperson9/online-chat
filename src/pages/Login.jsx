de40c1dd993b92e86a04889569072d0bbcad3ed1
Birdperson9 <yorieshkin@gmail.com>
2023-07-10 20:51:08 +0200

    add Login user functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,72 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuth } from '../utils/AuthContext'
 import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  const { user } = useAuth()
+  const { user, handleUserLogin } = useAuth()
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  })
+
   const navigate = useNavigate()
 
-  useEffect(() => {
-    if (user) {
-      navigate('/')
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (user) {
+  //     navigate('/')
+  //   }
+  // }, [])
 
-  return <div>Login</div>
+  const handleInputChange = (e) => {
+    let name = e.target.name
+    let value = e.target.value
+
+    setCredentials({ ...credentials, [name]: value })
+  }
+
+  return (
+    <div className='auth--container'>
+      <div className='form--wrapper'>
+        <form
+          onSubmit={(e) => {
+            handleUserLogin(e, credentials)
+          }}
+        >
+          <div className='field--wrapper'>
+            <label htmlFor='email'>Email:</label>
+            <input
+              required
+              type='email'
+              name='email'
+              placeholder='Enter your email...'
+              value={credentials.email}
+              onChange={(e) => {
+                handleInputChange(e)
+              }}
+            />
+          </div>
+
+          <div className='field--wrapper'>
+            <label>Password:</label>
+            <input
+              required
+              type='password'
+              name='password'
+              placeholder='Enter password...'
+              value={credentials.password}
+              onChange={(e) => {
+                handleInputChange(e)
+              }}
+            />
+          </div>
+
+          <div className='field--wrapper'>
+            <input className='btn btn--lg btn--main' type='submit' value='Login' />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Login
--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,12 +1,50 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect, useContext } from 'react'
+import { account } from '../appwriteConfig'
+import { useNavigate } from 'react-router-dom'
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(true)
 
-  const contextData = { user }
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    getUserOnLoad()
+  }, [])
+
+  const getUserOnLoad = async () => {
+    try {
+      const accountDetails = account.get()
+      setUser(accountDetails)
+    } catch (error) {
+      console.error(error)
+    }
+    setLoading(false)
+  }
+
+  const handleUserLogin = async (e, credentials) => {
+    e.preventDefault()
+
+    try {
+      const response = await account.createEmailSession(
+        credentials.email,
+        credentials.password
+      )
+      const accountDetails = account.get()
+      setUser(accountDetails)
+
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const contextData = {
+    user,
+    handleUserLogin,
+  }
 
   return (
     <AuthContext.Provider value={contextData}>
